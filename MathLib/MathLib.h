e80a04c122243bfbc5de083509564bed5ade0d07
Unknown <ShyWolfGames@gmail.com>
2019-08-01 13:54:13 -0400

    Initial Project Setup and some .git ignore file changes
    Initial Project Setup and some .git ignore file changes

---
+++ MathLib/MathLib.h
@@ -0,0 +1,3 @@
+#pragma once
+// MathLib
+// Creating a Math Library August 1, 2019 1:35PM EST
---
+++ Project00/MainApplication.cpp
@@ -0,0 +1,7 @@
+// Project00
+// Learning to program Games August 1, 2019 1:33PM EST
+#include <iostream>
+int main()
+{
+	return 0;
+}
