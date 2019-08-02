626be68e13c4037ae36ff89b5da556dddd772577
Unknown <ShyWolfGames@gmail.com>
2019-08-02 00:09:08 -0400

    Updated Project Files to VS2017
    Updated the Project files and started on the first project in the book

--- Project00/MainApplication.cpp
+++ Project00/MainApplication.cpp
@@ -1,7 +1,47 @@
 // Project00
 // Learning to program Games August 1, 2019 1:33PM EST
-#include <iostream>
-int main()
+//#include <iostream>
+//int main()
+//{
+//	return 0;
+//}
+
+//TODO left off at page 24 in the book
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+const char CLASS_NAME[] = "WinMain";
+LRESULT WINAPI WinProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
-	return 0;
+	switch (msg)
+	{
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	}
+	return DefWindowProc(hWnd, msg, wParam, lParam);
+}
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	
+
+	WNDCLASSEX wcx;
+	HWND hwnd;
+	wcx.cbSize = sizeof(wcx);
+	wcx.style = CS_HREDRAW | CS_VREDRAW;
+	wcx.lpfnWndProc = WinProc;
+	wcx.cbClsExtra = 0;
+	wcx.cbWndExtra = 0;
+	wcx.hInstance = hInstance;
+	wcx.hIcon = NULL;
+	wcx.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wcx.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+	wcx.lpszMenuName = NULL;
+	wcx.lpszClassName = CLASS_NAME;
+	wcx.hIconSm = NULL;
+
+	if (RegisterClassEx(&wcx) == 0)
+		return false;
+
+
+	
 }
