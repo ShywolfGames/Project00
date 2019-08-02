1dbc82e92f8838d6b06ac52dd103e03348e192ba
Unknown <ShyWolfGames@gmail.com>
2019-08-02 15:51:39 -0400

    Project 2 Text input
    Followed second tutorial app for text input

--- Project00/MainApplication.cpp
+++ Project00/MainApplication.cpp
@@ -10,6 +10,16 @@
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 const char CLASS_NAME[] = "WinMain";
+const char APP_TITLE[] = "Hello World";
+const int WINDOW_WIDTH = 400;
+const int WINDOW_HEIGHT = 400;
+
+HDC hdc;
+TCHAR ch = ' ';
+RECT rect;
+PAINTSTRUCT ps;
+HINSTANCE hinst;
+
 LRESULT WINAPI WinProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
 	switch (msg)
@@ -17,15 +27,34 @@ LRESULT WINAPI WinProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 	case WM_DESTROY:
 		PostQuitMessage(0);
 		return 0;
+	case WM_CHAR:
+		switch (wParam)
+		{
+		case 0x08:
+		case 0x09:
+		case 0x0A:
+		case 0x0D:
+		case 0x1B:
+			MessageBeep((UINT)-1);
+			return 0;
+		default:
+			ch = (TCHAR)wParam;
+			InvalidateRect(hWnd, NULL, TRUE);
+
+		}
+	case WM_PAINT:
+		hdc = BeginPaint(hWnd, &ps);
+		GetClientRect(hWnd, &rect);
+		TextOut(hdc, rect.right / 2, rect.bottom / 2, &ch, 1);
+		EndPaint(hWnd, &ps);
 	}
 	return DefWindowProc(hWnd, msg, wParam, lParam);
 }
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+bool CreateMainWIndow(HINSTANCE hInstance, int nCmdShow)
 {
-	
-
 	WNDCLASSEX wcx;
 	HWND hwnd;
+
 	wcx.cbSize = sizeof(wcx);
 	wcx.style = CS_HREDRAW | CS_VREDRAW;
 	wcx.lpfnWndProc = WinProc;
@@ -41,7 +70,35 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
 	if (RegisterClassEx(&wcx) == 0)
 		return false;
+	hwnd = CreateWindow(
+		CLASS_NAME, APP_TITLE, WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, WINDOW_WIDTH, WINDOW_HEIGHT, (HWND)NULL, (HMENU)NULL, hInstance, (LPVOID)NULL);
 
+	if (!hwnd)
+	{
+		return false;
+	}
+		
+	ShowWindow(hwnd, nCmdShow);
+	UpdateWindow(hwnd);
+	return true;
 
-	
+}
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	MSG msg;
+	if (!CreateMainWIndow(hInstance, nCmdShow))
+		return false;
+	int done = 0;
+	while (!done)
+	{
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+		{
+			if (msg.message == WM_QUIT)
+				done = 1;
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+
+		}
+	}
+	return msg.wParam;
 }
