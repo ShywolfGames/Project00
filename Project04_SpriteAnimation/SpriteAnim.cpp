798f6133f86365b3be5d1d26594b0799ae890894
Unknown <ShyWolfGames@gmail.com>
2019-08-19 21:40:40 -0400

    Sprite Animation and Input Control
    Finished the sprite animation and implemented input control for the project to control the ship

--- Project04_SpriteAnimation/SpriteAnim.cpp
+++ Project04_SpriteAnimation/SpriteAnim.cpp
@@ -18,18 +18,68 @@ void SpriteAnim::initialize(HWND hw)
 		throw(GameError(gameErrorNS::FATAL_ERROR, "error loading nebula  texture"));
 	if (!planetTexture.initialize(graphics, PLANET_IMAGE))
 		throw(GameError(gameErrorNS::FATAL_ERROR, "error loading planet  texture"));
+	if (!shipTexture.initialize(graphics, SHIP_IMAGE))
+		throw(GameError(gameErrorNS::FATAL_ERROR, "error loading ship  texture"));
 	if (!nebula.initialize(graphics, 0,0,0,&nebulaTexture))
 		throw(GameError(gameErrorNS::FATAL_ERROR, "error initializing nebula"));
 	if (!planet.initialize(graphics, 0,0,0,&planetTexture))
 		throw(GameError(gameErrorNS::FATAL_ERROR, "error initializing planet"));
-
 	planet.setX(GAME_WIDTH*0.5f - planet.getWidth()*0.5f);
 	planet.setY(GAME_HEIGHT*0.5f - planet.getHeight()*0.5f);
+	
+	if (!ship.initialize(graphics, SHIP_WIDTH, SHIP_HEIGHT, SHIP_COLS, &shipTexture))
+		throw(GameError(gameErrorNS::FATAL_ERROR, "error initializing planet"));
+	ship.setX(GAME_WIDTH / 4);
+	ship.setY(GAME_HEIGHT / 4);
+	ship.setFrames(SHIP_START_FRAME, SHIP_END_FRAME);
+	ship.setCurrentFrame(SHIP_START_FRAME);
+	ship.setFrameDelay(SHIP_ANIMATION_DELAY);
+	
+
+
 	return;	
 }
 
 void SpriteAnim::update()
 {
+	ship.update(frameTime);
+	//ship.setDegrees(ship.getDegrees() + frameTime * ROTATION_RATE);
+	//ship.setScale(ship.getScale() - frameTime * SCALE_RATE);
+	if (input->isKeyDown(SHIP_RIGHT_KEY))
+	{
+		ship.setX(ship.getX() + frameTime * SHIP_SPEED);
+		if (ship.getX() > GAME_WIDTH)
+		{
+			ship.setX((float)-ship.getWidth());
+		}
+	}
+	if (input->isKeyDown(SHIP_LEFT_KEY))
+	{
+		ship.setX(ship.getX() - frameTime * SHIP_SPEED);
+		if (ship.getX() < -ship.getX())
+		{
+			ship.setX((float)GAME_WIDTH);
+		}
+	}	
+	if (input->isKeyDown(SHIP_UP_KEY))
+	{
+		ship.setY(ship.getY() - frameTime * SHIP_SPEED);
+		if (ship.getY() < -ship.getY())
+		{
+			ship.setY((float)GAME_HEIGHT);
+
+		}
+	}	
+	if (input->isKeyDown(SHIP_DOWN_KEY))
+	{
+		ship.setY(ship.getY() + frameTime * SHIP_SPEED);
+		if (ship.getY() > GAME_HEIGHT)
+		{
+			ship.setY((float)-ship.getHeight());
+	
+		}
+	}
+
 }
 
 void SpriteAnim::ai()
@@ -45,11 +95,13 @@ void SpriteAnim::render()
 	graphics->spriteBegin();
 	nebula.draw();
 	planet.draw();
+	ship.draw();
 	graphics->spriteEnd();
 }
 
 void SpriteAnim::releaseAll()
 {
+	shipTexture.onLostDevice();
 	planetTexture.onLostDevice();
 	nebulaTexture.onLostDevice();
 	Game::releaseAll();
@@ -57,6 +109,7 @@ void SpriteAnim::releaseAll()
 
 void SpriteAnim::resetAll()
 {
+	shipTexture.onResetDevice();
 	nebulaTexture.onResetDevice();
 	planetTexture.onResetDevice();
 	Game::resetAll();
--- Project04_SpriteAnimation/SpriteAnim.h
+++ Project04_SpriteAnimation/SpriteAnim.h
@@ -7,14 +7,33 @@
 #include"..\Engine\image.h"
 const char NEBULA_IMAGE[] = "pictures\\orion.jpg";  
 const char PLANET_IMAGE[] = "pictures\\planet.png"; 
+const char SHIP_IMAGE[] = "pictures\\ship.png";
+
+const int SHIP_START_FRAME = 0;
+const int SHIP_END_FRAME = 3;
+const float SHIP_ANIMATION_DELAY = 0.2f;
+const int SHIP_COLS = 2;
+const int SHIP_WIDTH = 32;
+const int SHIP_HEIGHT = 32;
+const float ROTATION_RATE = 180.0f;
+const float SCALE_RATE = 0.2f;
+const float SHIP_SPEED = 100.0f;
+const float SHIP_SCALE = 1.5f;
+
+const UCHAR SHIP_LEFT_KEY = VK_LEFT;     // left arrow
+const UCHAR SHIP_RIGHT_KEY = VK_RIGHT;    // right arrow
+const UCHAR SHIP_UP_KEY = VK_UP;       // up arrow
+const UCHAR SHIP_DOWN_KEY = VK_DOWN;     // down arrow
 
 class SpriteAnim : public Game
 {
 private:
 	TextureManager nebulaTexture;
 	TextureManager planetTexture;
+	TextureManager shipTexture;
 	Image planet;
 	Image nebula;
+	Image ship;
 
 public:
 	SpriteAnim();
