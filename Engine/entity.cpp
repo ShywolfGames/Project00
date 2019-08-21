04832968b334cfc5f5cf89096ccaea38715978dd
ShyWolfGames <ShyWolfGames@gmail.com>
2019-08-21 15:39:08 -0400

    Implementing Entity class
    Started implementing the Entity class

--- Engine/entity.cpp
+++ Engine/entity.cpp
@@ -1,1 +1,145 @@
 #include "entity.h"
+Entity::Entity()
+{
+	radius = 1.0;
+	edge.left = -1;
+	edge.top = -1;
+	edge.right = 1;
+	edge.bottom = 1;
+	mass = 1.0;
+	velocity.x = 0.0;
+	velocity.y = 0.0;
+	deltaV.x = 0.0;
+	deltaV.y = 0.0;
+	active = true;               
+	rotatedBoxReady = false;
+	collisionType = entityNS::CIRCLE;
+	health = 100;
+	gravity = entityNS::GRAVITY;
+}
+void Entity::update(float frameTime)
+{
+	velocity += deltaV;
+	deltaV.x = 0;
+	deltaV.y = 0;
+	Image::update(frameTime);
+	rotatedBoxReady = false;
+}
+
+bool Entity::initialize(Game * gamePtr, int width, int height, int ncols, TextureManager * textureM)
+{
+	input = gamePtr->getInput();
+	return (Image::initialize(gamePtr->getGraphics(), width, height, ncols, textureM));
+}
+
+void Entity::activate()
+{
+	active = true;
+}
+
+void Entity::ai(float frameTime, Entity & ent)
+{
+}
+bool Entity::collidesWith(Entity & ent, VECTOR2 & collisionVector)
+{
+	if(!active || !ent.getActive())
+	return false;
+	if (collisionType == entityNS::CIRCLE && ent.getCollisionType() == entityNS::CIRCLE)
+		return collideCircle(ent, collisionVector);
+	if (collisionType == entityNS::BOX && ent.getCollisionType() == entityNS::BOX)
+		return collideBox(ent, collisionVector);
+	if (collisionType != entityNS::CIRCLE && ent.getCollisionType() != entityNS::CIRCLE)
+		return collideRotateBox(ent, collisionVector);
+	else
+		if (collisionType == entityNS::CIRCLE)
+		{
+			bool collide = ent.collideRotatedBoxCircle(*this, collisionVector);
+			collisionVector *= -1;
+			return collide;
+		}
+		else
+			return collideRotatedBoxCircle(ent, collisionVector);
+	return false;
+}
+
+bool Entity::collideCircle(Entity & ent, VECTOR2 & collisionVector)
+{
+	distSquared = *getCenter() - *ent.getCenter();
+	distSquared.x = distSquared.x*distSquared.x;
+	distSquared.y = distSquared.y*distSquared.y;
+	sumRadiiSquared = (radius*getScale()) + (ent.radius*ent.getScale());
+	sumRadiiSquared *= sumRadiiSquared;
+	if (distSquared.x+ distSquared.y <= sumRadiiSquared)
+	{
+		collisionVector = *ent.getCenter() - *getCenter();
+	}
+	return false;
+}
+
+bool Entity::collideBox(Entity & ent, VECTOR2 & collisionVector)
+{
+	if (!active || !ent.getActive())
+		return false;
+	if ((getCenterX() + edge.right*getScale() >= ent.getCenterX() + ent.getEdge().left*ent.getScale()) &&
+		(getCenterX() + edge.left*getScale() <= ent.getCenterX() + ent.getEdge().right*ent.getScale()) &&
+		(getCenterY() + edge.bottom*getScale() >= ent.getCenterY() + ent.getEdge().top*ent.getScale()) &&
+		(getCenterY() + edge.top*getScale() <= ent.getCenterY() + ent.getEdge().bottom*ent.getScale()) )
+	{
+		collisionVector = *ent.getCenter() - *getCenter();
+		return true;
+	}
+	return false;
+}
+
+bool Entity::collideRotateBox(Entity & ent, VECTOR2 & collisionVector)
+{
+	computeRotatedBox();
+	ent.computeRotatedBox();
+	if (projectionsOverlap(ent) && ent.projectionsOverlap(*this))
+	{
+		collisionVector = *ent.getCenter() - *getCenter();
+	}
+	return false;
+}
+
+bool Entity::collideRotatedBoxCircle(Entity & ent, VECTOR2 & collisionVector)
+{
+	return false;
+}
+
+void Entity::computeRotatedBox()
+{
+}
+
+bool Entity::projectionsOverlap(Entity & ent)
+{
+	return false;
+}
+
+bool Entity::collideCornerCircle(VECTOR2 corner, Entity & ent, VECTOR2 & collisionVector)
+{
+	return false;
+}
+
+bool Entity::collidePixelPerfect(Entity & ent, VECTOR2 & collisionVector)
+{
+	return false;
+}
+
+bool Entity::outsideRect(RECT rect)
+{
+	return false;
+}
+
+void Entity::damage(int weapon)
+{
+}
+
+void Entity::bounce(VECTOR2 & collisionVector, Entity & ent)
+{
+}
+
+void Entity::gravityForce(Entity * other, float frameTime)
+{
+}
+
--- Engine/entity.h
+++ Engine/entity.h
@@ -11,12 +11,97 @@ namespace entityNS
 	const float GRAVITY = 6.67428e-11f;
 }
 
-class entity:public Image
+class Entity : public Image
 {
 protected:
 	entityNS::COLLISION_TYPE collisionType;
 	VECTOR2 center;
 	float radius;
+	VECTOR2 distSquared;    
+	float   sumRadiiSquared;
+	RECT    edge;           
+	VECTOR2 corners[4];     
+	VECTOR2 edge01, edge03;  
+	float   edge01Min, edge01Max, edge03Min, edge03Max; 
+	VECTOR2 velocity;      
+	VECTOR2 deltaV;         
+	float   mass;         
+	float   health;         
+	float   rr;             
+	float   force;         
+	float   gravity;      
+	Input   *input;        
+	HRESULT hr;           
+	bool    active;         
+	bool    rotatedBoxReady;   
+	virtual bool collideCircle(Entity &ent, VECTOR2 &collisionVector);
+	virtual bool collideBox(Entity &ent, VECTOR2 &collisionVector);
+	virtual bool collideRotateBox(Entity &ent, VECTOR2 &collisionVector);
+	virtual bool collideRotatedBoxCircle(Entity &ent, VECTOR2 &collisionVector);
+	void computeRotatedBox();
+	bool projectionsOverlap(Entity &ent);
+	bool collideCornerCircle(VECTOR2 corner, Entity &ent, VECTOR2 &collisionVector);
+	bool collidePixelPerfect(Entity &ent, VECTOR2 &collisionVector);
+public:
+	Entity();
+	virtual ~Entity() {}
+	virtual const VECTOR2* getCenter()
+	{
+		center = VECTOR2(getCenterX(), getCenterY());
+		return &center;
+	}
+
+	// Return radius of collision circle.
+	virtual float getRadius() const { return radius; }
+
+	// Return RECT structure used for BOX and ROTATED_BOX collision detection.
+	virtual const RECT& getEdge() const { return edge; }
+
+	// Return corner c of ROTATED_BOX
+	virtual const VECTOR2* getCorner(UINT c) const
+	{
+		if (c >= 4)
+			c = 0;
+		return &corners[c];
+	}
+
+	// Return velocity vector.
+	virtual const VECTOR2 getVelocity() const { return velocity; }
+
+	// Return active.
+	virtual bool  getActive()         const { return active; }
+
+	// Return mass.
+	virtual float getMass()           const { return mass; }
+
+	// Return gravitational constant.
+	virtual float getGravity()        const { return gravity; }
+
+	// Return health;
+	virtual float getHealth()         const { return health; }
+
+	// Return collision type (NONE, CIRCLE, BOX, ROTATED_BOX)
+	virtual entityNS::COLLISION_TYPE getCollisionType() { return collisionType; }
+
+	virtual void  setVelocity(VECTOR2 v) { velocity = v; }
+	virtual void  setDeltaV(VECTOR2 dv) { deltaV = dv; }
+	virtual void  setActive(bool a) { active = a; }
+	virtual void setHealth(float h) { health = h; }
+	virtual void  setMass(float m) { mass = m; }
+	virtual void  setGravity(float g) { gravity = g; }
+	virtual void setCollisionRadius(float r) { radius = r; }
+
+	virtual void update(float frameTime);
+	virtual bool initialize(Game *gamePtr, int width, int height, int ncols,
+		TextureManager *textureM);
+	virtual void activate();
+	virtual void ai(float frameTime, Entity &ent);
+	virtual bool outsideRect(RECT rect);
+	virtual bool collidesWith(Entity &ent, VECTOR2 &collisionVector);
+	virtual void damage(int weapon);
+	void bounce(VECTOR2 &collisionVector, Entity &ent);
+	void gravityForce(Entity *other, float frameTime);
+
 
 };
 #endif // !_ENTITY_H
